ec2d284342c892e9380a7104fa1c3cffe970c065
Aljosha Friemann <a.friemann@automate.wtf>
2017-08-23 11:22:40 +0200

    added deprecation decorator

---
+++ simple_tools/decorators/warnings.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+.. module: TODO
+    :platform: TODO
+    :synopsis: TODO
+
+.. moduleauthor:: Aljosha Friemann a.friemann@automate.wtf
+"""
+
+import warnings, functools
+
+def deprecated(function):
+    @functools.wraps(function)
+    def deprecated_function(*args, **kwargs):
+        warnings.warn(
+            "function {} has been deprecated".format( function.__name__),
+            DeprecationWarning, stacklevel=2
+        )
+        return function(*args, **kwargs)
+    return deprecated_function
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
