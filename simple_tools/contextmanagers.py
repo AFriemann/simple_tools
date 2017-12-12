339fad4570e68d64f38614f20884ece76c18a22a
Aljosha Friemann <a.friemann@automate.wtf>
2017-12-12 16:19:06 +0100

    chdir contextmanager

--- simple_tools/contextmanagers.py
+++ simple_tools/contextmanagers.py
@@ -10,6 +10,7 @@
 
 from contextlib import contextmanager
 
+import os
 import time
 
 @contextmanager
@@ -18,5 +19,12 @@ def timer(name):
     yield
     elapsedTime = time.time() - startTime
     print('[{}] finished in {} ms'.format(name, int(elapsedTime * 1000)))
+
+@contextmanager
+def chdir(path):
+    old_dir = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_dir)
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4 fenc=utf-8
